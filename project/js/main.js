ca0fd055173799bb61db4f047d72fbea14896785
kobayasi929 <85878478+kobayasi929@users.noreply.github.com>
2022-07-15 07:33:32 +0900

    Add files via upload

--- project/js/main.js
+++ project/js/main.js
@@ -30,3 +30,46 @@ $(function(){
     );
 
 });
+
+//ここから
+function twoDigit(num) {
+    let ret;
+    if( num < 10 ) 
+      ret = "0" + num; 
+    else 
+      ret = num; 
+    return ret;
+  }
+  function showClock() {
+  var hiduke=new Date(); 
+
+  //年・月・日・曜日を取得する
+  var year = hiduke.getFullYear();
+  var month = hiduke.getMonth()+1;
+  var week = hiduke.getDay();
+  var day = hiduke.getDate();
+
+  var yobi= new Array("日","月","火","水","木","金","土");
+    document.getElementById("realtime").innerHTML = year+"年"+month+"月"+day+"日 "+yobi[week]+"曜日";
+  }
+  setInterval('showClock()',100);
+
+  $(function() {
+    var topBtn = $('#page-top');   
+    topBtn.hide();
+    //スクロールが100pxに達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スクロールしてトップ
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+});
